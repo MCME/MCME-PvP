dcde75b74ffb8e7c5003a771a3fcee46e8487a51
Jubo30 <julian.bornemann@gmx.net>
2022-10-10 22:15:50 +0200

    More for new Conquest

--- src/main/java/com/mcmiddleearth/mcme/pvp/Gamemode/Conquest.java
+++ src/main/java/com/mcmiddleearth/mcme/pvp/Gamemode/Conquest.java
@@ -1,19 +1,32 @@
 package com.mcmiddleearth.mcme.pvp.Gamemode;
 
+import com.mcmiddleearth.mcme.pvp.Handlers.GearHandler;
+import com.mcmiddleearth.mcme.pvp.PVP.PlayerStat;
 import com.mcmiddleearth.mcme.pvp.PVP.Team;
 import com.mcmiddleearth.mcme.pvp.PVPPlugin;
 import com.mcmiddleearth.mcme.pvp.Util.EventLocation;
+import com.mcmiddleearth.mcme.pvp.command.PVPCommand;
 import com.mcmiddleearth.mcme.pvp.maps.Map;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.plugin.PluginManager;
+import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  *
@@ -21,7 +34,7 @@ import java.util.HashMap;
  */
 public class Conquest extends BasePluginGamemode {
 
-    private int target = 900; // 15 mins
+    private int time = 15;
 
     private boolean pvpRegistered = false;
 
@@ -38,6 +51,11 @@ public class Conquest extends BasePluginGamemode {
     }));
 
     private int area = 1;
+
+    private int redScore = 3;
+    private int blueScore = 0;
+
+    private final int capturePointRadius = 3;
 
     private GameState state;
 
@@ -60,13 +78,125 @@ public class Conquest extends BasePluginGamemode {
 
     public Conquest(){ state = GameState.IDLE; }
 
+    Runnable tickCQ = new Runnable() {
+        @Override
+        public void run() {
+            time--;
+            if(time % 60 == 0){
+                Points.setDisplayName("Time: "+(time / 60) + "m");
+            }else if(time < 60 ){
+                Points.setDisplayName("Time: "+ time + "s");
+            }
+            if(time == 30){
+                for(Player player : Bukkit.getOnlinePlayers()){
+                    player.sendMessage(ChatColor.GREEN+"30 seconds remaining!");
+                }
+            }else if(time <= 10 && time > 1){
+                for(Player p : Bukkit.getOnlinePlayers()){
+                    p.sendMessage(ChatColor.GREEN + String.valueOf(time) + " seconds remaining!");
+                }
+            }else if(time == 1){
+                for(Player p : Bukkit.getOnlinePlayers()){
+                    p.sendMessage(ChatColor.GREEN + String.valueOf(time) + " second remaining!");
+                }
+            }
+            if(time == 0){
+                redTeamWin();
+            }
+        }
+    };
+
+    Runnable flagPoints = new Runnable() {
+        @Override
+        public void run() {
+            boolean captured = false;
+            if(area == 4){
+                blueTeamWin();
+            }
+            if(GMHandlers.capAmount.get("CapturePoint"+area) == 100){
+                //Tea, captured
+                area++;
+                captured = true;
+                for(Player player : Bukkit.getOnlinePlayers()){
+                    player.sendMessage(ChatColor.BLUE+"Blue team captured the next point.");
+                }
+                //Block b = e.getClickedBlock().getLocation().add(0, 1, 0).getBlock();
+                Block b = GMHandlers.points.get("CapturePoint"+area).add(0,1,0).getBlock();
+                b.setType(Material.BLUE_STAINED_GLASS);
+                blueScore--;
+                redScore++;
+                Points.getScore(ChatColor.BLUE+"Blue team flags:").setScore(blueScore);
+                Points.getScore(ChatColor.RED+"Red team flags:").setScore(redScore);
+            }else if(GMHandlers.capAmount.containsKey("CapturePoint"+(area-1))&&GMHandlers.capAmount.get("CapturePoint"+(area-1)) == -100){
+                //Tea, captured
+                area--;
+                captured = true;
+                for(Player player : Bukkit.getOnlinePlayers()){
+                    player.sendMessage(ChatColor.RED+"Red team recaptured the point.");
+                }
+                Block b = GMHandlers.points.get("CapturePoint"+(area-1)).add(0,1,0).getBlock();
+                b.setType(Material.RED_STAINED_GLASS);
+                blueScore++;
+                redScore--;
+                Points.getScore(ChatColor.BLUE+"Blue team flags:").setScore(blueScore);
+                Points.getScore(ChatColor.RED+"Red team flags:").setScore(redScore);
+            }
+            if(!captured) {
+                if (GMHandlers.blueTeamCaptureAttack.size() < GMHandlers.redTeamCaptureAttack.size()) {
+                    GMHandlers.capAmount.replace("CapturePoint"+area,GMHandlers.capAmount.get("CapturePoint"+area)+3);
+                } else if (GMHandlers.blueTeamCaptureAttack.size() == GMHandlers.redTeamCaptureAttack.size()) {
+                    //do nothing
+                } else if (GMHandlers.blueTeamCaptureAttack.size() > GMHandlers.redTeamCaptureAttack.size()) {
+                    GMHandlers.capAmount.replace("CapturePoint"+area,GMHandlers.capAmount.get("CapturePoint"+area)-3);
+                }
+                if(GMHandlers.capAmount.containsKey("CapturePoint"+(area-1))) {
+                    if (GMHandlers.blueTeamCaptureDef.size() < GMHandlers.redTeamCaptureDef.size()) {
+                        GMHandlers.capAmount.replace("CapturePoint" + area, GMHandlers.capAmount.get("CapturePoint" + area) + 3);
+                    } else if (GMHandlers.blueTeamCaptureDef.size() == GMHandlers.redTeamCaptureDef.size()) {
+                        //do nothing
+                    } else if (GMHandlers.blueTeamCaptureDef.size() > GMHandlers.redTeamCaptureDef.size()) {
+                        GMHandlers.capAmount.replace("CapturePoint" + area, GMHandlers.capAmount.get("CapturePoint" + area) - 3);
+                    }
+                }
+                for(Player player : GMHandlers.blueTeamCaptureAttack){
+                    if(GMHandlers.capAmount.get("CapturePoint"+area) >= 0){
+                        player.sendMessage(ChatColor.BLUE+"Cap at "+GMHandlers.capAmount.get("CapturePoint"+area)+"%");
+                    }else{
+                        player.sendMessage(ChatColor.RED+"Cap at "+GMHandlers.capAmount.get("CapturePoint"+area)*-1+"%");
+                    }
+                }
+                for(Player player : GMHandlers.redTeamCaptureAttack){
+                    if(GMHandlers.capAmount.get("CapturePoint"+area) >= 0){
+                        player.sendMessage(ChatColor.BLUE+"Cap at "+GMHandlers.capAmount.get("CapturePoint"+area)+"%");
+                    }else{
+                        player.sendMessage(ChatColor.RED+"Cap at "+GMHandlers.capAmount.get("CapturePoint"+area)*-1+"%");
+                    }
+                }
+                for(Player player : GMHandlers.blueTeamCaptureDef){
+                    if(GMHandlers.capAmount.get("CapturePoint"+area) >= 0){
+                        player.sendMessage(ChatColor.BLUE+"Cap at "+GMHandlers.capAmount.get("CapturePoint"+(area-1))+"%");
+                    }else{
+                        player.sendMessage(ChatColor.RED+"Cap at "+GMHandlers.capAmount.get("CapturePoint"+(area-1))*-1+"%");
+                    }
+                }
+                for(Player player : GMHandlers.redTeamCaptureDef){
+                    if(GMHandlers.capAmount.get("CapturePoint"+area) >= 0){
+                        player.sendMessage(ChatColor.BLUE+"Cap at "+GMHandlers.capAmount.get("CapturePoint"+(area-1))+"%");
+                    }else{
+                        player.sendMessage(ChatColor.RED+"Cap at "+GMHandlers.capAmount.get("CapturePoint"+(area-1))*-1+"%");
+                    }
+                }
+            }
+        }
+    };
+
     @Override
     public void Start(Map m, int parameter){
         count = PVPPlugin.getCountdownTime();
         state = GameState.COUNTDOWN;
         super.Start(m,parameter);
         this.map = m;
-        target = parameter;
+        time = parameter;
 
         if(!map.getImportantPoints().keySet().containsAll(NeededPoints)){
             for(Player p : players){
@@ -81,12 +211,134 @@ public class Conquest extends BasePluginGamemode {
             pm.registerEvents(GMHandlers,PVPPlugin.getPlugin());
             pvpRegistered = true;
         }
-        for(Player p:players){
-            if (Team.getBlue().size() <= Team.getBlue().size()){
-                Team.getRed().add(p);
-                p.teleport(m.getImportantPoints().get("Redspawn1").toBukkitLoc().add(0,2,0));
-                freezePlayer(p,140);
+        for(Location l : GMHandlers.points.values()){
+            l.getBlock().setType(Material.BEACON);
+
+            l.getBlock().getRelative(0, -1, -1).setType(Material.IRON_BLOCK);
+            l.getBlock().getRelative(0, -1, 0).setType(Material.IRON_BLOCK);
+            l.getBlock().getRelative(0, -1, 1).setType(Material.IRON_BLOCK);
+            l.getBlock().getRelative(1, -1, -1).setType(Material.IRON_BLOCK);
+            l.getBlock().getRelative(1, -1, 0).setType(Material.IRON_BLOCK);
+            l.getBlock().getRelative(1, -1, 1).setType(Material.IRON_BLOCK);
+            l.getBlock().getRelative(-1, -1, -1).setType(Material.IRON_BLOCK);
+            l.getBlock().getRelative(-1, -1, 0).setType(Material.IRON_BLOCK);
+            l.getBlock().getRelative(-1, -1, 1).setType(Material.IRON_BLOCK);
+        }
+        for(Player p : Bukkit.getOnlinePlayers()) {
+            if(players.contains(p)){
+                if (Team.getBlue().size() <= Team.getBlue().size()){
+                    Team.getRed().add(p);
+                    p.teleport(m.getImportantPoints().get("Redspawn1").toBukkitLoc().add(0,2,0));
+                    freezePlayer(p,140);
+                }else if(Team.getBlue().size() < Team.getRed().size()){
+                    Team.getBlue().add(p);
+                    p.teleport(m.getImportantPoints().get("Bluespawn1").toBukkitLoc().add(0,2,0));
+                    freezePlayer(p,140);
+                }
+            } else{
+                Team.getSpectator().add(p);
+                p.teleport(m.getSpawn().toBukkitLoc().add(0,2,0));
             }
+        }
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(PVPPlugin.getPlugin(), new Runnable() {
+            @Override
+            public void run() {
+                if(count == 0){
+                    if(state == GameState.RUNNING){
+                        return;
+                    }
+
+                    Bukkit.getScheduler().scheduleSyncRepeatingTask(PVPPlugin.getPlugin(),tickCQ,0,20);
+                    Bukkit.getScheduler().scheduleSyncRepeatingTask(PVPPlugin.getPlugin(),flagPoints,0,20);
+
+                    Points = getScoreboard().registerNewObjective("Score","dummy");
+                    Points.setDisplayName("Time: " + time + "m");
+                    time *= 60;
+                    Points.getScore(ChatColor.BLUE+"Blue team flags:").setScore(blueScore);
+                    Points.getScore(ChatColor.RED+"Red team flags:").setScore(redScore);
+                    Points.setDisplaySlot(DisplaySlot.SIDEBAR);
+                    state = GameState.RUNNING;
+                }else if(count != -1){
+                    for(Player p : Bukkit.getServer().getOnlinePlayers()){
+                        p.sendMessage(ChatColor.GREEN+"Game begins in "+count);
+                    }
+                    count--;
+                }
+            }
+        },40,20);
+    }
+
+    public void End(Map m){
+        state = GameState.IDLE;
+
+        getScoreboard().clearSlot(DisplaySlot.SIDEBAR);
+        m.playerLeaveAll();
+        PVPCommand.queueNextGame();
+        super.End(m);
+    }
+
+    private void redTeamWin(){
+        for(Player player : Bukkit.getOnlinePlayers()){
+            player.sendMessage(ChatColor.BLUE+"Game over!");
+            player.sendMessage(ChatColor.BLUE+"Red team wins!");
+        }
+        PlayerStat.addGameWon(Team.Teams.RED);
+        PlayerStat.addGameLost(Team.Teams.BLUE);
+        PlayerStat.addGameSpectatedAll();
+        End(map);
+    }
+
+    private void blueTeamWin(){
+        for(Player player : Bukkit.getOnlinePlayers()){
+            player.sendMessage(ChatColor.BLUE+"Game over!");
+            player.sendMessage(ChatColor.BLUE+"Blue team wins!");
+        }
+        PlayerStat.addGameWon(Team.Teams.BLUE);
+        PlayerStat.addGameLost(Team.Teams.RED);
+        PlayerStat.addGameSpectatedAll();
+        End(map);
+    }
+
+    @Override
+    public void playerLeave(Player p){ Team.removeFromTeam(p);}
+
+    public boolean midgamePlayerJoin(Player p){
+        if(state == GameState.RUNNING || state == GameState.COUNTDOWN){
+
+            if(Team.getRed().getAllMembers().contains(p)){
+                addToTeam(p, Team.Teams.RED);
+            }
+            else if(Team.getBlue().getAllMembers().contains(p)){
+                addToTeam(p, Team.Teams.BLUE);
+            }
+
+            else{
+                if(Team.getRed().size() >= Team.getBlue().size()){
+                    addToTeam(p, Team.Teams.BLUE);
+                }
+                else{
+                    addToTeam(p, Team.Teams.RED);
+                }
+            }
+            super.midgamePlayerJoin(p);
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    private void addToTeam(Player player, Team.Teams team){
+        //work with area
+        if(team == Team.Teams.RED){
+            Team.getRed().add(player);
+            player.teleport(map.getImportantPoints().get("Redspawn"+String.valueOf(area)).toBukkitLoc().add(0,2,0));
+            GearHandler.giveGear(player,ChatColor.RED, GearHandler.SpecialGear.NONE);
+        }else{
+            Team.getBlue().add(player);
+            player.teleport(map.getImportantPoints().get("Bluespawn"+String.valueOf(area)).toBukkitLoc().add(0,2,0));
+            GearHandler.giveGear(player,ChatColor.BLUE, GearHandler.SpecialGear.NONE);
         }
     }
 
@@ -96,14 +348,82 @@ public class Conquest extends BasePluginGamemode {
 
     private class GamemodeHandlers implements Listener {
         private HashMap<String, Location> points = new HashMap<>();
-        private HashMap<Location,Integer> capAmount = new HashMap<>();
+        private HashMap<String,Integer> capAmount = new HashMap<>();
+        private List<Player> redTeamCaptureAttack = new ArrayList<>();
+        private List<Player> blueTeamCaptureAttack = new ArrayList<>();
+        private List<Player> redTeamCaptureDef = new ArrayList<>();
+        private List<Player> blueTeamCaptureDef = new ArrayList<>();
+
 
         public GamemodeHandlers(){
 
             for(java.util.Map.Entry<String, EventLocation> e: map.getImportantPoints().entrySet()){
                 if(e.getKey().contains("Point")){
                     points.put(e.getKey(),e.getValue().toBukkitLoc());
-                    capAmount.put(e.getValue().toBukkitLoc(),0);
+                    capAmount.put(e.getKey(),-100);
+                }
+            }
+        }
+
+        @EventHandler
+        public void onPlayerMove(PlayerMoveEvent event){
+            // check if player enters area of beacon
+            Player player = event.getPlayer();
+            Location loc = player.getLocation();
+            if(loc.distance(map.getImportantPoints().get("CapturePoint"+String.valueOf(area)).toBukkitLoc())>capturePointRadius){
+                if(Team.getRed().getMembers().contains(event.getPlayer())){
+                    redTeamCaptureAttack.add(player);
+                }else if(Team.getBlue().getMembers().contains(event.getPlayer())){
+                    blueTeamCaptureAttack.add(player);
+                }
+            }
+            if(area != 1){
+                if(loc.distance(map.getImportantPoints().get("CapturePoint"+String.valueOf(area-1)).toBukkitLoc())>capturePointRadius){
+                    if(Team.getRed().getMembers().contains(event.getPlayer())){
+                        redTeamCaptureDef.add(player);
+                    }else{
+                        blueTeamCaptureDef.add(player);
+                    }
+                }
+            }
+        }
+
+        @EventHandler
+        public void onPlayerDeath(PlayerDeathEvent event){
+            if(state == GameState.RUNNING){
+                if(event.getEntity() instanceof Player){
+                    Player player = (Player) event.getEntity();
+                    if(Team.getRed().getMembers().contains(player)){
+                        redTeamCaptureAttack.remove(player);
+                    }else if(Team.getBlue().getMembers().contains(player)){
+                        blueTeamCaptureAttack.remove(player);
+                    }
+                }
+            }
+        }
+
+        @EventHandler
+        public void onPlayerLeave(PlayerQuitEvent event){
+            if(state == GameState.RUNNING || state == GameState.COUNTDOWN){
+                Team.removeFromTeam(event.getPlayer());
+                if(Team.getRed().size() <= 0){ // Maybe needs changing
+                    blueTeamWin();
+                }
+                if(Team.getBlue().size() <= 0){ // Maybe needs changing
+                    redTeamWin();
+                }
+            }
+        }
+
+        @EventHandler
+        public void onPlayerRespawn(PlayerRespawnEvent event){
+            if(state == GameState.RUNNING){
+                if(Team.getBlue().getMembers().contains(event.getPlayer())){
+                    event.setRespawnLocation(map.getImportantPoints().get("Bluespawn"
+                            +String.valueOf(area)).toBukkitLoc().add(0,2,0));
+                }else if(Team.getRed().getMembers().contains(event.getPlayer())){
+                    event.setRespawnLocation(map.getImportantPoints().get("Redspawn"
+                            +String.valueOf(area)).toBukkitLoc().add(0,2,0));
                 }
             }
         }
--- src/main/java/com/mcmiddleearth/mcme/pvp/command/PVPCommand.java
+++ src/main/java/com/mcmiddleearth/mcme/pvp/command/PVPCommand.java
@@ -120,7 +120,7 @@ public class PVPCommand extends CommandDispatcher<Player>{
                     doCommand("kickPlayer", c.getArgument("player", String.class), c.getSource());
                     return 1;} )))
             .then(LiteralArgumentBuilder.<Player>literal("rules")
-                    .then(RequiredArgumentBuilder.<Player, String>argument("gamemode", new CommandStringArgument("infected", "teamslayer", "teamdeathmatch", "ringbearer", "oneinthequiver", "teamconquest", "deathrun", "capturetheflag","egghunt")).executes(c -> {
+                    .then(RequiredArgumentBuilder.<Player, String>argument("gamemode", new CommandStringArgument("infected", "teamslayer", "teamdeathmatch", "ringbearer", "oneinthequiver", "teamconquest", "deathrun", "capturetheflag","egghunt","conquest")).executes(c -> {
                         doCommand("rules", c.getArgument("gamemode", String.class), c.getSource());
                         return 1;} )))
             .then(LiteralArgumentBuilder.<Player>literal("pipe").executes(c -> {
@@ -174,7 +174,7 @@ public class PVPCommand extends CommandDispatcher<Player>{
                             return 1;
                         })))
                 .then(LiteralArgumentBuilder.<Player>literal("gm")
-                    .then(RequiredArgumentBuilder.<Player, String>argument("gm", new CommandStringArgument("FreeForAll", "Infected", "OneInTheQuiver", "Ringbearer", "TeamConquest", "TeamDeathmatch", "TeamSlayer", "DeathRun", "CaptureTheFlag","EggHunt")).executes(c -> {
+                    .then(RequiredArgumentBuilder.<Player, String>argument("gm", new CommandStringArgument("FreeForAll", "Infected", "OneInTheQuiver", "Ringbearer", "TeamConquest", "TeamDeathmatch", "TeamSlayer", "DeathRun", "CaptureTheFlag","EggHunt","Conquest")).executes(c -> {
                             doCommand("mapEditorGm", c.getArgument("map", String.class), c.getArgument("gm", String.class), c.getSource());
                             return 1;
                         })))
@@ -599,6 +599,9 @@ public class PVPCommand extends CommandDispatcher<Player>{
                     case "egghunt":
                         source.sendMessage(ChatColor.GREEN + "Egg Hunt Rules");
                         source.sendMessage(ChatColor.GRAY + "Around the map, there are several eggs (wool blocks). Each one is worth a different amount of points. To 'take' the egg, right click on it, and a certain points will be added to your score. However, if you die, points will be reducted. Happy Hunting!");
+                    case "conquest":
+                        source.sendMessage(ChatColor.GREEN+"Conquest");
+                        source.sendMessage(ChatColor.GRAY+"Test");
                 }
                 break;
             case "deleteMap":
